82ca7c0e8bc5f4383d1340a8716dee9f55847be7
campside <chirkovv.ma@yandex.ru>
2018-04-08 19:37:28 +0300

    Задание 1

---
+++ test_create_group.py
@@ -0,0 +1,44 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+success = True
+wd = WebDriver()
+wd.implicitly_wait(60)
+
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+def test_create_group():
+    try:
+        wd.get("http://localhost/")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("New group")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("Test")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("test")
+        wd.find_element_by_name("submit").click()
+        wd.find_element_by_link_text("group page").click()
+        wd.find_element_by_css_selector("span.group").click()
+        wd.find_element_by_link_text("Logout").click()
+    finally:
+        wd.quit()
+        if not success:
+            raise Exception("Test failed.")
