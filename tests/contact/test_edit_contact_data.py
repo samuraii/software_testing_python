14a70aaedc019a57dfd32aa8eba8af04f60b3608
campside <chirkovv.ma@yandex.ru>
2018-04-19 22:13:17 +0300

    Небольшие правки по кодстайлу

--- tests/contact/test_edit_contact_data.py
+++ tests/contact/test_edit_contact_data.py
@@ -1,8 +1,9 @@
 import random
 from model.data import Contact
 
+
 def test_edit_group_data(app):
     app.session.login('admin', 'secret')
-    name  = random.randint(1, 100)
+    name = random.randint(1, 100)
     app.contact.edit_data(Contact(firstname='firstname{}'.format(name), lastname='lastname', nickname='nickname{}'.format(name)))
     app.session.logout()
